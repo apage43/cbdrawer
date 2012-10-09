a7ed495c9745178fa0856a4a62a28d5e9dc8fa53
Aaron Miller <apage43@ninjawhale.com>
2012-10-08 23:41:53 -0700

    Restructure and stuff
     * Moved transcoders into cbdrawer.transcoders
     * Moved ops into .client, now it should only be necessary to require
       cbdrawer.client to get everything needed for basic kv-usage
     * Removed specific with- macros for transcoders, replaced with a single
       generic one

--- src/cbdrawer/client.clj
+++ src/cbdrawer/client.clj
@@ -1,32 +1,10 @@
 (ns cbdrawer.client
-  (:require [cheshire.core :as json])
+  (:refer-clojure :exclude [get set!])
+  (:require [cbdrawer.transcoders :as xcoders])
   (:import [com.couchbase.client CouchbaseClient CouchbaseConnectionFactory]
            [net.spy.memcached.transcoders Transcoder]
-           [net.spy.memcached CachedData]
+           [net.spy.memcached CachedData CASMutator CASMutation]
            [java.net URI]))
-
-(def 
-  ^{:doc
- "A spymemcached Transcoder that serializes Clojure datastructures to
-  JSON, which allows use of Couchbase Views."}
-  json-transcoder
-  (proxy [Transcoder] []
-    (asyncDecode [_] false)
-    (decode [bs] (json/parse-string (String. (.getData bs)) true))
-    (encode [o] (CachedData. 0 (.getBytes (json/encode o)) CachedData/MAX_SIZE))
-    (getMaxSize [] CachedData/MAX_SIZE)))
-
-(def 
-  ^{:doc
- "A spymemcached Transcoder that serializes Clojure datastructures.
-  The default java Serializing transcoder will work for any Clojure type,
-  but this format is smaller and readable. See edn-format.org"}
-  clj-transcoder
-  (proxy [Transcoder] []
-    (asyncDecode [_] false)
-    (decode [bs] (read-string (String. (.getData bs))))
-    (encode [o] (CachedData. 0 (.getBytes (pr-str o)) CachedData/MAX_SIZE))
-    (getMaxSize [] CachedData/MAX_SIZE)))
 
 (defn factory
   "Create a CouchbaseConnectionFactory from a cluster base URI.
@@ -37,17 +15,90 @@
 (defn client
   "Open a CouchbaseClient.
    (client \"bucketname\" \"password\" \"http://localhost:8091/\") or (client factory)"
-  ([factory]
+  ([^CouchbaseConnectionFactory factory]
    (CouchbaseClient. factory))
   ([bucket password & uris]
    (CouchbaseClient. (mapv #(.resolve (URI. %) "/pools") uris)  bucket password)))
 
 (defn shutdown
   "Shut down a CouchbaseClient"
-  ([client] (.shutdown client))
-  ([client timeout unit] (.shutdown client timeout unit)))
+  ([^CouchbaseClient client] (.shutdown client))
+  ([^CouchbaseClient client timeout unit] (.shutdown client timeout unit)))
 
 (defn capi-bases
   "Get the Couch-API base URLs from a cluster/bucket."
   [^CouchbaseConnectionFactory connectionfactory]
   (vec (.getCouchServers (.getVBucketConfig connectionfactory))))
+
+(def ^:dynamic ^Transcoder *transcoder* xcoders/clj-transcoder)
+
+(defn set-transcoder!
+  "Globally reset the default transcoder."
+  [transcoder]
+  (alter-var-root #'*transcoder* (fn [_] transcoder)))
+
+
+(defmacro with-transcoder
+  "Serialize and deserialize items in this block using the specified transcoder.
+  (with-transcoder my-transcoder exprs)"
+  [xcoder & body]
+  `(binding [*transcoder* ~xcoder]
+     ~@body))
+
+(defn derefable-future
+  "Wrap a java.util.concurrent.Future so it can be used with deref/@"
+  [^java.util.concurrent.Future fut]
+  (reify
+    clojure.lang.IDeref
+     (deref [_] (.get fut))
+    java.util.concurrent.Future
+     (get [_ timeout unit] (.get fut timeout unit))
+     (isCancelled [_] (.isCancelled fut))
+     (isDone [_] (.isDone fut))
+     (cancel [_ interrupt?] (.cancel fut interrupt?))))
+
+(defn- cas-with-transcoder!
+  "Atomically update an item with the result of applying `f` to it,
+   with a specified transcoder."
+  [^CouchbaseClient conn key f ^Transcoder transcoder]
+     (let [mutation (proxy [CASMutation] []
+                      (getNewValue [current]
+                        (f current)))]
+       (.cas (CASMutator. conn transcoder) key nil 0 mutation)))
+
+(defn cas!
+  "Atomically update an item with f and additional args. Returns the new value."
+  [^CouchbaseClient conn k f & args]
+  (cas-with-transcoder! conn (name k) #(apply (partial f %) args) *transcoder*))
+
+(defn get
+  "Get an item, synchronously"
+  [^CouchbaseClient conn k]
+  (.get conn (name k) *transcoder*))
+
+(defn get-async
+  "Get an item asynchronously. Returns the result in a future"
+  [^CouchbaseClient conn k]
+  (derefable-future (.asyncGet conn (name k) *transcoder*)))
+
+(defn delete!
+  "Delete an item. Returns a boolean indicating whether the operation succeeded
+  in a future"
+  [^CouchbaseClient conn k]
+  (derefable-future (.delete conn (name k))))
+
+(defn add!
+  "Create an item iff it doesn't already exist. Returns a boolean indicating
+  whether the operation succeeded in a future"
+  ([^CouchbaseClient conn k value ^long expiration] 
+   (derefable-future (.add conn (name k) expiration value *transcoder*)))
+  ([conn k value]
+   (add! conn k value 0)))
+
+(defn force!
+  "Update the value of an item, creating it if it does not exist. Returns a
+  boolean indicating whether the operation succeeded in a future"
+  ([^CouchbaseClient conn k value ^long expiration] 
+   (derefable-future (.set conn (name k) expiration value *transcoder*)))
+  ([conn k value]
+   (force! conn k value 0)))
---
+++ src/cbdrawer/transcoders.clj
@@ -0,0 +1,42 @@
+(ns cbdrawer.transcoders
+  (:require [cheshire.core :as cheshire])
+  (:import [net.spy.memcached.transcoders Transcoder]
+           [net.spy.memcached CachedData]))
+
+(def 
+  ^{:doc
+ "A spymemcached Transcoder that serializes Clojure datastructures to
+  JSON, which allows use of Couchbase Views."}
+  json-transcoder
+  (proxy [Transcoder] []
+    (asyncDecode [_] false)
+    (decode [bs] (cheshire/parse-string (String. (.getData bs)) true))
+    (encode [o] (CachedData. 0 (.getBytes (cheshire/encode o)) CachedData/MAX_SIZE))
+    (getMaxSize [] CachedData/MAX_SIZE)))
+
+(def 
+  ^{:doc
+ "A spymemcached Transcoder that serializes to SMILE, a JSON-compatible compact
+ binary format. http://wiki.fasterxml.com/SmileFormatSpec"}
+  smile-transcoder
+  (proxy [Transcoder] []
+    (asyncDecode [_] false)
+    (decode [bs] (cheshire/parse-smile (.getData bs) true))
+    (encode [o] (CachedData. 0 (cheshire/encode-smile o) CachedData/MAX_SIZE))
+    (getMaxSize [] CachedData/MAX_SIZE)))
+
+(def 
+  ^{:doc
+ "A spymemcached Transcoder that serializes Clojure datastructures.
+  The default java Serializing transcoder will work for any Clojure type,
+  but this format is smaller and readable. See edn-format.org"}
+  clj-transcoder
+  (proxy [Transcoder] []
+    (asyncDecode [_] false)
+    (decode [bs] (read-string (String. (.getData bs))))
+    (encode [o] (CachedData. 0 (.getBytes (pr-str o)) CachedData/MAX_SIZE))
+    (getMaxSize [] CachedData/MAX_SIZE)))
+
+(def ^{:doc "The default spymemcached serializing transcoder"} spy-transcoder
+  (net.spy.memcached.transcoders.SerializingTranscoder.))
+
